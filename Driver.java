b2834e5e61301debe099f394e5451d0156c6757d
Hriday Algh <hridayalgh@gmail.com>
2023-06-08 15:07:46 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,14 +1,25 @@
+import java.io.File;
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        double [] c1 = {6,3,4,5,-3};
+        int [] e1 = {0,3,2,7, 4};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double [] c2 = {1,-2,-2,8,-9};
+        int [] e2 = {1,3,5,7,9};
+        Polynomial p2 = new Polynomial(c2,e2);
         Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        System.out.println("s(1) = " + s.evaluate(1));
         if(s.hasRoot(1)) System.out.println("1 is a root of s");
         else System.out.println("1 is not a root of s");
+
+        s = p1.multiply(p2);
+        s.saveToFile("s.p");
+        System.out.println(s.evaluate(2.0));
+        System.out.println(s.evaluate(2.0)== (p1.evaluate(2.0)*p2.evaluate(2.0)));
+
+        Polynomial n = new Polynomial(new File("s.p"));
+        n.saveToFile("s2.p");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,36 +1,120 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileWriter;
 class Polynomial {
     double[] coefficients;
+    int[] exponents;
 
     Polynomial() {
         coefficients = new double[]{0.0};
+        exponents = new int[]{0};
     }
 
-    Polynomial(double[] c) {
+    Polynomial(double[] c, int[] e) {
         coefficients = c;
+        exponents = e;
+    }
+
+    Polynomial(File f) throws Exception {
+        Polynomial p = new Polynomial();
+        Scanner s = new Scanner(f);
+        s.useDelimiter("");
+        while (s.hasNext()) {
+            String t = s.next();
+            System.out.println(s.hasNext("[+-]"));
+            while (s.hasNext() && !s.hasNext("-") && !s.hasNext("\\+")) { t += s.next(); System.out.println(t);}
+            String[] l = t.split("x");
+            double[] c = new double[]{Double.parseDouble(l[0])};
+            int[] e;
+            if (l.length == 2) e = new int[]{Integer.parseInt(l[1])};
+            else e = new int[]{0};
+            p = p.add(new Polynomial(c,e));
+        }
+        coefficients = p.coefficients;
+        exponents = p.exponents;
+    }
+
+    void simplify() {
+        int l = 0;
+        for (int i = 0; i < exponents.length; i++) {
+            for (int j = i + 1; j < exponents.length; j++) {
+                if (exponents[i] == exponents[j]) {
+                    coefficients[j] += coefficients[i];
+                    coefficients[i] = 0.0;
+                    break;
+                }
+            }
+            if (coefficients[i] != 0) l++;
+        }
+
+        if (l == 0) {
+            coefficients = new double[]{0.0};
+            exponents = new int[]{0};
+        } else {
+            double[] c = new double[l];
+            int[] e = new int[l];
+            for (int i = 0; i < coefficients.length; i++)
+                if (coefficients[i] != 0) {
+                    c[--l] = coefficients[i];
+                    e[l] = exponents[i];
+                }
+            coefficients = c;
+            exponents = e;
+        }
     }
 
     Polynomial add(Polynomial o) {
         int i;
-        double[] n = new double[Math.max(coefficients.length, o.coefficients.length)];
+        double[] c = new double[coefficients.length + o.coefficients.length];
+        int [] e = new int[exponents.length + o.exponents.length];
 
-        for (i = 0; i < n.length; i++) n[i] = 0;
 
-        for (i = 0; i < coefficients.length; i++) n[i] += coefficients[i];
+        for (i = 0; i < coefficients.length; i++) c[i] = coefficients[i];
+        for (i = 0; i < o.coefficients.length; i++) c[coefficients.length + i] = o.coefficients[i];
+        for (i = 0; i < exponents.length; i++) e[i] = exponents[i];
+        for (i = 0; i < o.exponents.length; i++) e[i + exponents.length] = o.exponents[i];
 
-        for (i = 0; i < o.coefficients.length; i++) n[i] += o.coefficients[i];
+        Polynomial p =  new Polynomial(c,e);
+        p.simplify();
+        return p;
+    }
 
-        return new Polynomial(n);
+    Polynomial multiply(Polynomial o) {
+        double[] c = new double[coefficients.length * o.coefficients.length];
+        int[] e = new int[exponents.length * o.exponents.length];
+        for (int i = 0; i < coefficients.length; i++) {
+            for (int j = 0; j < o.coefficients.length; j++) {
+                c[i*o.coefficients.length + j] = coefficients[i]*o.coefficients[j];
+                e[i*o.exponents.length + j] = exponents[i] + o.exponents[j];
+            }
+        }
+        Polynomial n = new Polynomial(c,e);
+        n.simplify();
+        return n;
     }
 
     double evaluate(double x) {
         double sum = 0.0;
         for (int i = 0; i < coefficients.length; i++) {
-            sum += coefficients[i] * Math.pow(x, i);
+            sum += coefficients[i] * Math.pow(x, exponents[i]);
         }
         return sum;
     }
 
     boolean hasRoot(double x) {
         return this.evaluate(x) == 0;
+    }
+    
+    void saveToFile(String s) throws Exception {
+        File f = new File(s);
+        f.createNewFile();
+        FileWriter output = new FileWriter(f);
+
+        for (int i = 0; i < coefficients.length; i++) {
+            if (coefficients[i] > 0 && i!=0) output.write("+");
+            output.write(String.valueOf(coefficients[i]));
+            if (exponents[i]!=0) output.write("x" + String.valueOf(exponents[i]));
+        }
+        output.close();
     }
 }
